1fea4edbd3a723d4593a8ffe4c46ed4ebb16c5e1
Mitch <mitch@hawkins5.com>
2024-08-11 22:32:42 +1000

    Added Simple Rectangle to GUI, Initialised gamethread boilerplate

--- src/GUI.java
+++ src/GUI.java
@@ -1,30 +1,52 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GUI {
+public class GUI extends JPanel implements Runnable {
+
+  JFrame frame;
 
   public GUI() {
-    JFrame frame = new JFrame();
+    frame = new JFrame();
 
-    JPanel panel = new JPanel();
-    panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 300, 300));
-    panel.setLayout(new GridLayout(0, 1));
-    panel.setBackground(Color.BLACK);
+    this.setBorder(BorderFactory.createEmptyBorder(300, 300, 300, 300));
+    this.setLayout(new GridLayout(0, 1));
+    this.setBackground(Color.BLACK);
 
-    frame.add(panel, BorderLayout.CENTER);
+    frame.add(this, BorderLayout.CENTER);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setTitle("Our GUI");
-    frame.pack();
+    frame.setSize(600, 600);
     frame.setVisible(true);
-
-    Thread gameThread;
   }
 
-  public static void main(String[] args) throws Exception {
-    new GUI();
+  Thread gameThread;
+
+  public void startGameThread() {
+    gameThread = new Thread(this);
+    gameThread.start();
+  }
+
+  public void run() {
+    while (gameThread != null) {
+      update();
+      repaint(); // calls paint component, a little bit confusing though.
+    }
+  }
+
+  public void update() {}
+
+  public void paintComponent(Graphics g) {
+    System.out.println("I drew something");
+    super.paintComponent(g); // This is boiler plate for paintComponent method (subclass of JPanel)
+    Graphics2D g2 = (Graphics2D) g;
+    g2.setColor(Color.blue);
+    g2.fillRect(100, 100, 100, 100);
+    g2.dispose();
   }
 }
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    GUI GUI = new GUI();
+    GUI.startGameThread();
+  }
+}
